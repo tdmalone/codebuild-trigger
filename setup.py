96cf3c0a0f7280b2d21b975e28d0e1d698f278bd
Tim Malone <tdmalone@gmail.com>
2018-01-28 20:12:48 +1100

    Version bump

--- setup.py
+++ setup.py
@@ -3,12 +3,12 @@ from distutils.core import setup
 setup(
   name = 'codebuild-trigger',
   packages = ['codebuild-trigger'],
-  version = '0.0.3',
+  version = '0.0.4',
   description = 'An AWS Lambda function to triggers CodeBuild on pushes to CodeCommit.',
   author = 'Tim Malone',
   author_email = 'tdmalone@gmail.com',
   url = 'https://github.com/tdmalone/codebuild-trigger',
-  download_url = 'https://github.com/tdmalone/codebuild-trigger/archive/v0.0.3.tar.gz',
+  download_url = 'https://github.com/tdmalone/codebuild-trigger/archive/v0.0.4.tar.gz',
   keywords = ['aws', 'lambda', 'codebuild', 'codecommit'],
   classifiers = [],
 )
